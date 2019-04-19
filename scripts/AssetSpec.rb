982d850f16e92069698f2d9680bdfdba7f798a96
maustinstar <maustinstar@gmail.com>
2019-04-18 21:47:37 -0400

    add option for generating all vs missing assets

--- scripts/AssetSpec.rb
+++ scripts/AssetSpec.rb
@@ -9,6 +9,10 @@ require_relative 'utils/sips'
 require_relative 'utils/files'
 require_relative 'utils/find_largest'
 
+# read argument to generate all assets
+# defaults to true
+generate_all = ARGV[0] == "-a"
+
 # Find all image assets
 for filename in Dir["**/*.imageset/*.json"] do
   
@@ -22,12 +26,14 @@ for filename in Dir["**/*.imageset/*.json"] do
   
   # generate all images
   for image in json["images"] do
+    next if image["filename"] == large["filename"]
+    next if !generate_all and !image["filename"].nil?
     scale = image["scale"].to_f
-    newsize = (width * scale).to_i.to_s
     out = "#{dir filename}/#{lastdir filename, '.*'}@#{scale.to_i.to_s}#{ext large['filename']}"
-    scale_to_width largename, newsize, out
+    scale_to_width largename, (width * scale).to_i.to_s, out
     # add json reference to new asset
     image["filename"] = base out, ''
+    puts out
   end
   
   # update asset json
@@ -36,7 +42,7 @@ end
   
 # Find all AppIcon assets
 for filename in Dir["**/*.appiconset/*.json"] do
-    
+  
   # json object for image set
   json = parse filename
   # json object for largest image in set
@@ -44,11 +50,14 @@ for filename in Dir["**/*.appiconset/*.json"] do
   
   # generate all images
   for image in json["images"] do
+    next if image["filename"] == large["filename"]
+    next if !generate_all and !image["filename"].nil?
     newsize = (((image["size"]).to_s.split 'x').first.to_f * image["scale"].to_f).to_i.to_s
     out = "#{dir filename}/#{lastdir filename, '.*'}@#{newsize}#{ext large['filename']}"
     scale_to_width "#{dir filename}/#{large['filename']}", newsize, out
     # add json reference to new asset
     image["filename"] = base out, ''
+    puts out
   end
   
   # update asset json
