307e711aeba654384b4c5880193b07c24b405588
maustinstar <maustinstar@gmail.com>
2019-04-18 21:26:13 -0400

    correct spacing

--- scripts/AssetSpec.rb
+++ scripts/AssetSpec.rb
@@ -13,7 +13,7 @@ require_relative 'utils/find_largest'
 for filename in Dir["**/*.imageset/*.json"] do
   
   # json object for image set
-  json  = parse filename
+  json = parse filename
   # json object for largest image in set
   large = find_largest filename, json
   largename = "#{dir filename}/#{large['filename']}"
@@ -24,7 +24,7 @@ for filename in Dir["**/*.imageset/*.json"] do
   for image in json["images"] do
     scale = image["scale"].to_f
     newsize = (width * scale).to_i.to_s
-    out  = "#{dir filename}/#{lastdir filename, '.*'}@#{scale.to_i.to_s}#{ext large['filename']}"
+    out = "#{dir filename}/#{lastdir filename, '.*'}@#{scale.to_i.to_s}#{ext large['filename']}"
     scale_to_width largename, newsize, out
     # add json reference to new asset
     image["filename"] = base out, ''
@@ -36,15 +36,16 @@ end
   
 # Find all AppIcon assets
 for filename in Dir["**/*.appiconset/*.json"] do
+    
   # json object for image set
-  json  = parse filename
+  json = parse filename
   # json object for largest image in set
   large = find_largest filename, json
   
   # generate all images
   for image in json["images"] do
     newsize = (((image["size"]).to_s.split 'x').first.to_f * image["scale"].to_f).to_i.to_s
-    out  = "#{dir filename}/#{lastdir filename, '.*'}@#{newsize}#{ext large['filename']}"
+    out = "#{dir filename}/#{lastdir filename, '.*'}@#{newsize}#{ext large['filename']}"
     scale_to_width "#{dir filename}/#{large['filename']}", newsize, out
     # add json reference to new asset
     image["filename"] = base out, ''
